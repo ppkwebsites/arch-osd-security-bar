52ee6df8141ac61ff8458f01d6754ef532fb1bf7
Peter <31070043+ppkwebsites@users.noreply.github.com>
2026-03-02 15:32:07 +0200

    Delete SecurityBar.sh

--- SecurityBar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if pgrep -f "quickshell.*SecurityBar/SecurityBar.qml" >/dev/null; then
-    pkill -f "quickshell.*"SecurityBar/SecurityBar.qml
-else
-    QT_QUICK_BACKEND=software quickshell -p ~/.config/quickshell/SecurityBar/SecurityBar.qml &
-fi
