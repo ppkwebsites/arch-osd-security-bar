6c2e79229cef128a415c575178afcdaddaa0bbda
Peter <31070043+ppkwebsites@users.noreply.github.com>
2026-03-02 15:32:38 +0200

    Delete NetworkSecurityBar.sh

--- NetworkSecurityBar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if pgrep -f "quickshell.*NetworkSecurityBar/NetworkSecurityBar.qml" >/dev/null; then
-    pkill -f "quickshell.*NetworkSecurityBar/NetworkSecurityBar.qml"
-else
-    QT_QUICK_BACKEND=software quickshell -p ~/.config/quickshell/NetworkSecurityBar/NetworkSecurityBar.qml &
-fi
